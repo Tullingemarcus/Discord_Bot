4cccbe0df70fe48184c3e2c3387ee5ba8952b3cc
unknown <marcus@tullingeskog.se>
2020-03-05 11:30:14 +0100

    ny klocka + rensa upp i filerna

--- STAT_BOT/bot.js
+++ STAT_BOT/bot.js
@@ -2,26 +2,15 @@ const Discord = require('discord.js-commando');
 const auth = require('./auth.json');
 const bot = new Discord.Client();
 
+
 bot.registry.registerGroup('core', 'Core');
 bot.registry.registerDefaults();
+bot.registry.registerGroup('basic', 'Basic');
+
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
 bot.on('ready',function(){
 	console.log("ready")
 });
-
-
-bot.on('message', message=> {
-	var prefix = '§'
-	var msg = message.content;
-	
-	if (msg === prefix + 'hallå') {
-		message.channel.send('hejsan' + message.author, {});
-	}
-	if (msg === prefix + 'tjena') {
-		message.channel.send('/tts', {});
-	}
-});
-
 
 bot.login(auth.token);
